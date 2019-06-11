447b5b79aaca4c7d06985e26a99d356b817bc2c6
NoharaMasato <masato338914914@gmail.com>
2019-06-11 20:14:37 +0900

    initial-commit

---
+++ CheckBookers.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then #引数が一つではなかった場合
+  echo "実行するコマンドが違います" 1>&2
+  echo "githubのurlをコマンドライン引数として設定してください" 1>&2
+  echo "ex) './CheckBookers.sh https://github.com/NoharaMasato/RspecBookers2'"
+  exit 1
+fi
+
+readonly GITHUB_URL=$1
+
+#dir_name=bookers1${bookers1_num}
+dir_name=bookers
+
+cd .. #このリポジトリから抜ける
+rm -rf $dirname
+git clone $GITHUB_URL $dir_name
+
+cd $dir_name
+
+#ここでGemfile.lockとGemfileがあるかどうかの場合
+if [ -e Gemfile ]; then 
+  rm Gemfile.lock
+fi
+
+if [ -e Gemfile ]; then
+  if [ -e spec ]; then 
+    sed -i -e '/ruby.*/d' Gemfile # Gemfileのrubyのバージョンを変更
+    bundle install
+    rails db:migrate:reset RAILS_ENV=test
+    bundle exec rspec spec/ --format documentation
+  else
+    echo "git clone したディレクトリにspecディレクトリがありません"
+    exit 1;
+  fi
+else
+  echo "git clone したディレクトリにGemfileがありません"
+  exit 1;
+fi
